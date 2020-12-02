a1831832b2314ce4f9241e3c46877f13fe4c9702
TingxinXie <tingxin.xie@mail.utoronto.ca>
2020-12-03 00:08:22 +0800

    test

---
+++ app.config.js
@@ -0,0 +1,7 @@
+var env = {
+    "Env": "dev",
+    "API_URL": "https://www.easy-mock.com/mock/5cd26d3e587faa68f3339979/example",
+    "TEST_URL": "http://192.168.12.73:3000",
+    "VIDEO_URL": "http://npm.paic.com.cn:9090",
+    "webServer": "http://10.8.40.145:8200"
+}
---
+++ cloud/Dockerfile
@@ -0,0 +1,17 @@
+
+
+FROM nginx:latest
+MAINTAINER guoxi
+
+
+COPY . /usr/share/nginx/html/
+
+RUN mv /usr/share/nginx/html/cloud/nginx/* /etc/nginx/
+
+
+# EXPOSE 10089
+
+# CMD ["sh","-c","/etc/nginx/start.sh"]
+
+# EXPOSE 7080
+
